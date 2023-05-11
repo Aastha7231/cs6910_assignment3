65960d712e211943db0db85391ee0798200aefb2
Aastha7231 <126596782+Aastha7231@users.noreply.github.com>
2023-05-11 23:11:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -197,3 +197,65 @@ def variablesFromPairs(input_lang, output_lang, pairs, max_length):
         res.append((input_variable, output_variable))
         i+=1
     return res
+
+def train(input_tensor, output_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, configuration, max_length):
+    batch_size = configuration['batch_size']
+
+    encoder_hidden = encoder.initHidden(configuration['num_layers_encoder'])
+
+    if configuration["cell_type"] == "LSTM":
+        encoder_cell_state = encoder.initHidden(configuration['num_layers_encoder'])
+        encoder_hidden = (encoder_hidden, encoder_cell_state)
+
+    input_tensor = Variable(input_tensor.transpose(0, 1))
+    output_tensor = Variable(output_tensor.transpose(0, 1))
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    encoder_outputs = Variable(torch.zeros(max_length, batch_size, encoder.hidden_size))
+    encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
+
+    loss ,i = 0, 0
+
+    input_length = input_tensor.size(0)
+    output_length = output_tensor.size(0)
+
+    while(i < (input_length)):
+        encoder_output, encoder_hidden = encoder(input_tensor[i], encoder_hidden)
+        i+=1
+
+    decoder_input = Variable(torch.LongTensor([SOS_token]*batch_size))
+    decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing :
+        i=0
+        while(i < (output_length)) :
+            decoder_output, decoder_hidden= decoder(decoder_input, decoder_hidden)
+            decoder_input = output_tensor[i]
+            loss += criterion(decoder_output, output_tensor[i])
+            i+=1
+
+    else :
+        j=0
+        while(j < (output_length)) :
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            topv, topi = decoder_output.data.topk(1)
+            decoder_input = torch.cat(tuple(topi))
+
+            decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+            loss += criterion(decoder_output, output_tensor[j])
+            j+=1
+            
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / output_length
